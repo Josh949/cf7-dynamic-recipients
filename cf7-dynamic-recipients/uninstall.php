c6c2b1d99d2cf37a015ec296f48481c70473550c
Josh <40504854+Josh949@users.noreply.github.com>
2024-11-02 16:49:24 -0700

    Delete cf7-dynamic-recipients directory

--- cf7-dynamic-recipients/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-// If uninstall not called from WordPress, exit
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
-}
-
-// Delete plugin options
-delete_option('cf7_dynamic_recipients_settings');
